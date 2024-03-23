636080866d0fdc2cb616ea48032beab9b85012c2
GiovanniBSDuarte <giovannibsduarte@gmail.com>
2024-03-23 18:06:24 -0300

    broker metodos compra e venda

--- src/Broker.java
+++ src/Broker.java
@@ -1,9 +1,28 @@
- class Broker implements Runnable{
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DeliverCallback;
+import java.nio.charset.StandardCharsets;
+
+class Broker implements Runnable{
+
+	private static final String EXCHANGE_NAME = "topic_logs";
     public static void main(String[] args) {
         
         Thread t1 = new Thread(new Broker());
 
 		t1.start();
+		try {
+			compra("ABEV3", 10, 10);
+			venda("ABEV3", 6, 8);
+		} catch (IOException | TimeoutException e) {
+			e.printStackTrace();
+		}
+	
 
 		
     }
@@ -15,4 +34,37 @@
 		System.out.println("Programa em execucao...");
         
 	}
+
+	public static void compra(String corretora, int quant, double val) throws IOException, TimeoutException {
+		String topic = "compra."+corretora;
+		String message = corretora + ";" + quant + ";" + val;
+		enviaPedido(topic, message);
+	}
+	
+	public static void venda(String corretora, int quant, double val) throws IOException, TimeoutException {
+		String topic = "venda."+corretora;
+		String message = corretora + ";" + quant + ";" + val;
+		enviaPedido(topic, message);
+	}
+	
+	public static void enviaPedido(String topic, String message) throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("gull.rmq.cloudamqp.com");
+		factory.setUsername("zwzsdwdx");
+		factory.setPassword("dIPnl1KCfla3vDb6FzjDOLh30BP-mrtu");
+		factory.setVirtualHost("zwzsdwdx");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+	
+		channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+	
+		channel.basicPublish(EXCHANGE_NAME, topic, null, message.getBytes("UTF-8"));
+		System.out.println(" [x] Sent '" + topic + "':'" + message + "'");
+	
+		channel.close();
+		connection.close();
+	}
+	
+
+
 }
